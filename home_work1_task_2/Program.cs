a8674aac5b22049d6e2033f16a39fbc8d1c4aabf
Andrei <fmwar92@gmail.com>
2022-06-24 15:58:31 +0300

    created a program for finding the maximum and minimum of two numbers

--- home_work1_task_2/Program.cs
+++ home_work1_task_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the thirst number: ");
+int thirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (thirstNumber > secondNumber)
+{
+    Console.WriteLine("The thirst number (" + thirstNumber + ") is the maximum");
+    Console.WriteLine("The second number (" + secondNumber + ") is the minimum");
+}
+else
+{
+    Console.WriteLine("The second number (" + secondNumber + ") is the maximum");
+    Console.WriteLine("The thirst number (" + thirstNumber + ") is the minimum");
+}
