6b8816e0f8cdef20706a7f37cc219eaaf5fe2ac0
Andrei <fmwar92@gmail.com>
2022-06-24 17:12:58 +0300

    working with even and uneven numbers

---
+++ home_work1_task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter the thirst number: ");
+int thirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (thirstNumber > secondNumber)
+{
+    if (thirstNumber > thirdNumber)
+    {
+       Console.WriteLine("The thirst number (" + thirstNumber + ") is the maximum"); 
+    }
+    else
+    {
+        Console.WriteLine("The third number (" + thirdNumber + ") is the maximum");
+    }
+}
+else
+{
+    if (secondNumber > thirdNumber)
+    {
+        Console.WriteLine("The second number (" + secondNumber + ") is the maximum");
+    }
+    else
+    {
+        Console.WriteLine("The third number (" + thirdNumber + ") is the maximum"); 
+    } 
+}
---
+++ home_work1_task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int evenN = N % 2;
+
+if (evenN == 0)
+{
+    Console.WriteLine("The number (" + N + ") is even"); // четное число
+}
+else
+{
+    Console.WriteLine("The number (" + N + ") is uneven"); // нечетное число
+}
