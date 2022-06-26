53b2f7e003073326ed3adb748da4bb1b9a7179d1
Andrei <fmwar92@gmail.com>
2022-06-26 17:38:27 +0300

    Added an additional variable to max shortened the code

---
+++ home_work_task_4_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter the thirst number: ");
+int thirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = thirstNumber;
+
+if (secondNumber > maxNumber) maxNumber = secondNumber;
+if (thirdNumber > maxNumber) maxNumber = thirdNumber;
+{
+    Console.WriteLine("Maximum number is " + maxNumber);
+}
