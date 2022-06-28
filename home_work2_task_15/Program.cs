aa5da5c64a27e06d21bb0debd77a0174328800ee
Andrei <fmwar92@gmail.com>
2022-06-28 12:56:22 +0300

    The task is to check whether the day is a weekend

---
+++ home_work2_task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool WeekEnd (int number)
+{
+    return(number == 6 ^ number == 7);
+}
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine(WeekEnd(number));
+}
+else
+{
+    Console.WriteLine("Not the day of the week");
+}
