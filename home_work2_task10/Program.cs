67cc23286cbde0e51ba5556083cae48a4f70c1fc
Andrei <fmwar92@gmail.com>
2022-06-28 10:28:13 +0300

    Task to find second digit from three-digit number

---
+++ home_work2_task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int FindSecondDigit(int number)
+{
+    int secondDigit = (number % 100) / 10;
+    return secondDigit;
+}
+if (number >= 100 && number <= 999)
+{
+    Console.WriteLine(FindSecondDigit(number));
+}
+else
+{
+    Console.WriteLine("The number is not three-digit");
+}
