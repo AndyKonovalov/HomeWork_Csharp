52fe969e0abfb047982f4b2a356f18f71a52b810
Andrei <fmwar92@gmail.com>
2022-06-29 23:01:15 +0300

    Added another way to find the third digit. Used string index

---
+++ home_work_task_13_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 99)
+{
+    Console.WriteLine("There is no third digit");
+}
+else
+{
+    string str = number.ToString();
+    Console.WriteLine(str[2]);    
+}
