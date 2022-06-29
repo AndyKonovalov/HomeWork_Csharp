aafc49b09b4c0f69ddeb82337a89f0f76201c551
Andrei <fmwar92@gmail.com>
2022-06-29 13:52:56 +0300

    Added another way

--- home_work2_task_15/Program.cs
+++ home_work2_task_15/Program.cs
@@ -1,15 +1,33 @@
-﻿Console.Write("Enter a number: ");
+﻿//int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Enter a number: ");
+
+//bool WeekEnd (int number)
+//{
+//    return(number == 6 ^ number == 7);
+//}
+//if (number >= 1 && number <= 7)
+//{
+//    Console.WriteLine(WeekEnd(number));
+//}
+//else
+//{
+//    Console.WriteLine("Not the day of the week");
+//}
+Console.Write("Enter a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-bool WeekEnd (int number)
-{
-    return(number == 6 ^ number == 7);
-}
 if (number >= 1 && number <= 7)
 {
-    Console.WriteLine(WeekEnd(number));
+    if (number == 6 ^ number == 7)
+    {
+        Console.WriteLine("This is a day off");
+    }
+    else
+    {
+        Console.WriteLine("This is not a day off");
+    }
 }
 else
 {
-    Console.WriteLine("Not the day of the week");
+    Console.WriteLine("Not the day of the week"); // фича)
 }
