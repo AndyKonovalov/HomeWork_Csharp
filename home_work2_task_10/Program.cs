e1d4b1c82dbe1539a9bcdeff13fd09ef5d399c36
Andrei <fmwar92@gmail.com>
2022-06-29 22:48:22 +0300

    Another way to find second digit from three-digit number. Used string index

---
+++ home_work2_task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 1 && number <= 99)
+{
+   Console.WriteLine("The number is not three-digit");
+}
+else
+{
+string str = number.ToString();
+Console.WriteLine(str[1]);
+}
