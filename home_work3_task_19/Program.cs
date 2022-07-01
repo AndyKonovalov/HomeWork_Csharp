6efc5602d59e879f1137fcb27b9894092813cb41
Andrei <fmwar92@gmail.com>
2022-07-01 21:32:13 +0300

    found out how to find a palindrome

--- home_work3_task_19/Program.cs
+++ home_work3_task_19/Program.cs
@@ -1,7 +1,8 @@
-﻿Console.Write("Enter a five-digit number: ");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Enter a five-digit number: ");
 string number = Console.ReadLine();
 int numberLength = number.Length; // just like array.Length
-if (numberLength <= 4 ^ numberLength >= 6) // five-digit number length = 5
+if (numberLength <= 4 ^ numberLength >= 6) // five-digit number length = 5, feature
 {
     Console.WriteLine("The number is not five-digit");
 }
