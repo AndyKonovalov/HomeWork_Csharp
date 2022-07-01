672f1f9e72b8a8fcd6d32060e30e84b563411d07
Andrei <fmwar92@gmail.com>
2022-07-01 16:21:39 +0300

    Worked with strings

---
+++ home_work3_task_19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter a five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 9999 ^ number >= 100000)
+{
+    Console.WriteLine("The number is not five-digit");
+}
+string str = number.ToString();
+if (str[0] == str[4] && str [1] == str [3])
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+// if ((number / 10000 == number % 10) && ((number / 1000) % 10 == (number / 10) % 10))
+// {
+//     Console.WriteLine((number / 10000 == number % 10) && ((number / 1000) % 10 == (number / 10) % 10));
+// }
+// else
+// {
+//     Console.WriteLine((number / 10000 == number % 10) && ((number / 1000) % 10 == (number / 10) % 10));
+// }
