c5d93b8a6bda82c96bfa6c1caf7aeda2acbdc761
Andrei <fmwar92@gmail.com>
2022-07-01 16:53:19 +0300

    Worked with Math.Pow to find cube of numbers

---
+++ home_work3_task_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Enter a number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"The cube of the number {i} is: {Math.Pow(i,3)}");
+}
