f7f790cd6637a95fceda91a9ca836a80f3172ba6
Andrei <fmwar92@gmail.com>
2022-07-01 16:39:50 +0300

    Found distance between two points in 3D space

---
+++ home_work3_task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+Console.Write("Enter the first coordinate of A(x): ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second coordinate of A(y): ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third coordinate of A(z): ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the first coordinate of B(x): ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second coordinate of B(y): ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third coordinate of B(z): ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2) + Math.Pow(z1 - z2,2));
+Console.WriteLine($"The distance between points A and B is: {dist}");
