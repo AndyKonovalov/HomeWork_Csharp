7ef86c45db1595703fed422f9d44fac5fe5ad266
Andrei <fmwar92@gmail.com>
2022-07-06 16:00:01 +0300

    Found out how find sum of digits in numbers

---
+++ home_work4_task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Enter a number: ");
+string number = Console.ReadLine();
+int [] array = new int [number.Length];
+int sum = 0;
+
+for ( int i = 0; i < number.Length; i++)
+{
+    array[i] = int.Parse(number[i].ToString());
+}
+for (int i = 0; i < number.Length; i++)
+{
+    sum += array[i];
+}
+Console.WriteLine($"Sum of digits in a number {number} = {sum}");
