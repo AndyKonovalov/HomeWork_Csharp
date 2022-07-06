f5c5cfa66fc07f6480efb728d3f29855615e6004
Andrei <fmwar92@gmail.com>
2022-07-06 12:20:38 +0300

    Used cycle 'for' to find the value of variable A to the power of B

---
+++ home_work4_task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter number A: ");
+int numbA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B: ");
+int numbB = Convert.ToInt32(Console.ReadLine());
+double numbAPowerOfB = 0;
+
+for (int i = 0; i <= numbB; i++)
+{
+    numbAPowerOfB = Math.Pow(numbA, i);
+}
+Console.WriteLine($"The number A to the power of B is {numbAPowerOfB}");
+
+
