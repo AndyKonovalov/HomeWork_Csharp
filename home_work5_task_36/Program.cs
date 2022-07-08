a93018d36ebd79434baff43009dc21a102fbc9cd
Andrei <fmwar92@gmail.com>
2022-07-08 18:42:11 +0300

    Worked with arrays. Found sum of all odd index elements in the array

---
+++ home_work5_task_36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+Console.Write("Set the size of the array: ");
+string number = Console.ReadLine();
+if ((int.TryParse(number, out int size)) && (size > 0))
+{
+    int[] array = new int[size];
+    int sumOfOddIndexElements = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+        if (i % 2 != 0)
+        {
+            sumOfOddIndexElements = sumOfOddIndexElements + array[i];
+        }
+    }
+    Console.WriteLine($"Your array is [{String.Join("; ", array)}]. Sum of all odd index elements in the array = {sumOfOddIndexElements}");
+}
+else
+{
+    Console.WriteLine("Entered data is not correct. Please, use positive integers");
+}
