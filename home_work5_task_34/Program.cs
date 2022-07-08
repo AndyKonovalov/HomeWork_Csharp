5c457b5adae49a7c22e92e1ecf26df727a87869a
Andrei <fmwar92@gmail.com>
2022-07-08 18:15:03 +0300

    Worked with arrays. Found count of even elements

---
+++ home_work5_task_34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. Напишите 
+// программу, которая покажет количество чётных чисел в 
+// массиве.
+Console.Write("Set the size of the array: ");
+string number = Console.ReadLine();
+if (int.TryParse(number, out int size))
+{
+    int[] array = new int[size];
+    int countEvenNumbers = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        if (array[i] % 2 == 0)
+        {
+            countEvenNumbers++;
+        }
+    }
+    Console.WriteLine($"Your array is [{String.Join("; ", array)}]. Count of even numbers in the array = {countEvenNumbers}");
+}
+else
+{
+    Console.WriteLine("Entered data is not a number. Please, use integers");
+}
