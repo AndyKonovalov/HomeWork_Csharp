e67dfcafc5fba7f273c62f696d816335aeac0594
Andrei <fmwar92@gmail.com>
2022-07-09 08:57:38 +0300

    worked with arrays, found the sum of all odd index elements in the array

--- home_work5_task_36/Program.cs
+++ home_work5_task_36/Program.cs
@@ -7,13 +7,10 @@ if ((int.TryParse(number, out int size)) && (size > 0))
 {
     int[] array = new int[size];
     int sumOfOddIndexElements = 0;
-    for (int i = 0; i < size; i++)
+    for (int i = 1; i < size; i += 2)
     {
         array[i] = new Random().Next(-100, 101);
-        if (i % 2 != 0)
-        {
-            sumOfOddIndexElements = sumOfOddIndexElements + array[i];
-        }
+        sumOfOddIndexElements = sumOfOddIndexElements + array[i];    
     }
     Console.WriteLine($"Your array is [{String.Join("; ", array)}]. Sum of all odd index elements in the array = {sumOfOddIndexElements}");
 }
