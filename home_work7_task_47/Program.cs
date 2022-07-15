9871ebcf954ae398189f3c4c6f18bbb1ab1102f7
Andrei <fmwar92@gmail.com>
2022-07-15 15:14:03 +0300

    Создал матрицу из вещественных чисел

---
+++ home_work7_task_47/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Set the size of matrix. Set the count of rows: ");
+string countOfRows = Console.ReadLine();
+Console.Write("Set the size of matrix. Set the count of columns: ");
+string countOfColumns = Console.ReadLine();
+if (int.TryParse(countOfRows, out int rows) &&
+int.TryParse(countOfColumns, out int columns) &&
+rows > 1 && columns > 1)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * 10 - 10, 2);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+else Console.WriteLine("Please, use integers greater than '1'");
