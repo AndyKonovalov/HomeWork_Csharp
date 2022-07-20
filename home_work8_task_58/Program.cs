68de7690d3525ed6c3d2fa70725bf5baebaee570
Andrei <fmwar92@gmail.com>
2022-07-20 11:43:34 +0300

    Задача 58. Задать 2 матрицы и найти произведение матриц. Добавил проверки на ввод данных пользователем с консоли

--- home_work8_task_58/Program.cs
+++ home_work8_task_58/Program.cs
@@ -1,52 +1,63 @@
-﻿Console.Write("Введите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine());
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.Write("Введите количество строк массива: ");
+string inputRows = Console.ReadLine();
 Console.Write("Введите количество столбцов массива: ");
-int columns = int.Parse(Console.ReadLine());
-
-/// Создает двумерный массив m на n: m - кол-во строк, n - кол-во столбцов.
-int[,] FillArray(int m, int n)
+string inputColumns = Console.ReadLine();
+if (inputRows != "" && inputColumns != "")
 {
-    int[,] matrix = new int[m, n];
-    for (int i = 0; i < m; i++)
+    if (int.TryParse(inputRows, out int rows) && (int.TryParse(inputColumns, out int columns)))
     {
-        for (int j = 0; j < n; j++)
+        if (rows >= 2 && columns >= 2)
         {
-            matrix[i, j] = new Random().Next(11);
-        }
+            /// Создает двумерный массив m на n: m - кол-во строк, n - кол-во столбцов.
+            int[,] FillArray(int m, int n)
+            {
+                int[,] matrix = new int[m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = new Random().Next(11);
+                    }
+                }
+                return matrix;
+            }
+            /// Печатает массив на экран.
+            void PrintArray(int[,] inputArray)
+            {
+                for (int i = 0; i < inputArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < inputArray.GetLength(1); j++)
+                    {
+                        Console.Write(inputArray[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            /// Создает 3 матрицу, размером k на l: k - количество строк l - количество столбцов.
+            int[,] MultiplyMatrix(int[,] firstArray, int[,] secondArray, int k, int l)
+            {
+                int[,] resultMatrix = new int[k, l];
+                for (int i = 0; i < k; i++)
+                {
+                    for (int j = 0; j < l; j++)
+                    {
+                        resultMatrix[i, j] = firstArray[i, j] * secondArray[i, j];
+                    }
+                }
+                return resultMatrix;
+            }
+            Console.Write("\nДаны две матрицы. Необходимо найти произведение двух матриц.\n");
+            int[,] firstMatrix = FillArray(rows, columns);
+            PrintArray(firstMatrix);
+            Console.WriteLine();
+            int[,] secondMatrix = FillArray(rows, columns);
+            PrintArray(secondMatrix);
+            Console.WriteLine("Произведение двух матриц равно:");
+            int[,] thirdMatrix = MultiplyMatrix(firstMatrix, secondMatrix, rows, columns);
+            PrintArray(thirdMatrix);
+        }else Console.WriteLine("Количество строк и столбцов не может быть отрицательным и должно быть больше 1");
     }
-    return matrix;
+    else Console.WriteLine("Ошибка ввода. Некорректное число!");
 }
-/// Печатает массив на экран.
-void PrintArray(int[,] inputArray)
-{
-    for (int i = 0; i < inputArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inputArray.GetLength(1); j++)
-        {
-            Console.Write(inputArray[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-}
-/// Создает 3 матрицу, размером k на l: k - количество строк l - количество столбцов.
-int[,] MultiplyMatrix(int[,] firstArray, int[,] secondArray, int k, int l)
-{
-    int[,] resultMatrix = new int[k, l];
-    for (int i = 0; i < k; i++)
-    {
-        for (int j = 0; j < l; j++)
-        {
-            resultMatrix[i, j] = firstArray[i, j] * secondArray[i, j];
-        }
-    }
-    return resultMatrix;
-}
-Console.Write("\nДаны две матрицы. Необходимо найти произведение двух матриц.\n");
-int[,] firstMatrix = FillArray(rows, columns);
-PrintArray(firstMatrix);
-Console.WriteLine();
-int[,] secondMatrix = FillArray(rows, columns);
-PrintArray(secondMatrix);
-Console.WriteLine("Произведение двух матриц равно:");
-int[,] thirdMatrix = MultiplyMatrix(firstMatrix, secondMatrix, rows, columns);
-PrintArray(thirdMatrix);
+else Console.WriteLine("Ошибка ввода. Пустая строка");
