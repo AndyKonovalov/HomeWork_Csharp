66ee951c0e053c64f717c2522c04f57f23fe70e3
Andrei <fmwar92@gmail.com>
2022-07-20 13:06:31 +0300

    Задача 52. Найти среднее арифметическое элементов в каждом столбце матрицы

--- home_work7_task_52/Program.cs
+++ home_work7_task_52/Program.cs
@@ -1,42 +1,42 @@
 ﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Console.Write("Set the size of matrix. Set the count of rows: ");
-// string countOfRows = Console.ReadLine();
-// Console.Write("Set the size of matrix. Set the count of columns: ");
-// string countOfColumns = Console.ReadLine();
-// if (int.TryParse(countOfRows, out int rows) &&
-// int.TryParse(countOfColumns, out int columns) &&
-// rows > 1 && columns > 1)
-// {
-//     double[,] matrix = new double[rows, columns];
-//     double arithmeticAverage = 0;
-//     double[] averageArray = new double[columns];
-//     double sumColumn = 0;
-//     int arrIndex = 0;
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matrix[i, j] = new Random().Next(21);
-//             Console.Write(matrix[i, j] + "\t");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-//     for (int l = 0; l < matrix.GetLength(1); l++)
-//     {
-//         for (int k = 0; k < matrix.GetLength(0); k++)
-//         {
-//             sumColumn += matrix[k, l];
-//         }
-//         arithmeticAverage = sumColumn / rows;
-//         averageArray[arrIndex] = arithmeticAverage;
-//         arrIndex++;
-//         sumColumn = 0;
-//     }
-//     Console.Write($"Average of every column is: {String.Join("; ", averageArray)}");
+Console.Write("Set the size of matrix. Set the count of rows: ");
+string countOfRows = Console.ReadLine();
+Console.Write("Set the size of matrix. Set the count of columns: ");
+string countOfColumns = Console.ReadLine();
+if (int.TryParse(countOfRows, out int rows) &&
+int.TryParse(countOfColumns, out int columns) &&
+rows > 1 && columns > 1)
+{
+    double[,] matrix = new double[rows, columns];
+    double arithmeticAverage = 0;
+    double[] averageArray = new double[columns];
+    double sumColumn = 0;
+    int arrIndex = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(21);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    for (int l = 0; l < matrix.GetLength(1); l++)
+    {
+        for (int k = 0; k < matrix.GetLength(0); k++)
+        {
+            sumColumn += matrix[k, l];
+        }
+        arithmeticAverage = sumColumn / rows;
+        averageArray[arrIndex] = arithmeticAverage;
+        arrIndex++;
+        sumColumn = 0;
+    }
+    Console.Write($"Average of every column is: {String.Join("; ", averageArray)}");
 
-// }
-// else Console.WriteLine("Please, use integers greater than '1'");
+}
+else Console.WriteLine("Please, use integers greater than '1'");
 
 
 // double[,] matr = new double[4, 4];
