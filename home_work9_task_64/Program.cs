9704fb188a27f35cfe9c03dc7b8fd29539988603
Andrei <fmwar92@gmail.com>
2022-07-23 09:10:33 +0300

    3адача 64. Pабота с рекурсией, нахождение всех натуральных чисел из заданного промежутка от M до N

---
+++ home_work9_task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Write("Введите значение M: ");
+int valueM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int valueN = Convert.ToInt32(Console.ReadLine());
+
+if (valueM != valueN && valueM >= 1)
+{
+    string FindNumbers(int start, int end)
+    {
+        if (start == end) return start.ToString();
+        return (start + ", " + FindNumbers(start + 1, end));
+    }
+
+    Console.WriteLine(FindNumbers(valueM, valueN));
+} else Console.WriteLine("M не должно равняться N и должно быть больше 0");
+
